73eb863ae72458e30b268c9468acfd14a01ba2e5
killer Li <2304806342@qq.com>
2017-11-26 23:41:38 +0800

    php

---
+++ Login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+	</head>
+<body>
+	<div align="center">
+		<form method= "post" class="form-signin" action="logindo.php">
+			<div>
+				<h1>登陆</h1>
+			</div>
+			<div>
+				用户名：<input type="text" placeholder="在此输入用户名 " name='username' /> <br>
+			    密码：&nbsp;&nbsp;&nbsp;<input type="password" placeholder="在此输入密码" name='passcode' /> <br><br>
+				<input type="submit" name="submit" value="登陆"/><br>
+				<div >
+					未注册? <a  href="registration.php"> 点击注册 </a>
+				</div>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+	</head>
+<body>
+<div align="center">
+    <form method= "post" action="registerdo.php">
+        <div >
+            <h1 >注册</h1>
+        </div>
+        <div >
+            <p> 输入你的信息</p>
+            <div>
+				用户名：<input type="text" placeholder="在此输入用户名 " name='username' /> <br>
+			    密码：&nbsp;&nbsp;&nbsp;<input type="password" placeholder="在此输入密码" name='passcode' /> <br>
+					<br>
+				<input type="submit" name="submit" value="注册">
+       </div>
+    </form>
+</div>
+</body>
+</html>
